a06be6cb0c2a292c803a1ddc98c5cc08bba62603
xzorkiix <xzorkiix@gmail.com>
2018-03-11 00:53:47 +0300

    add getters and setters

--- src/Entity/Department.php
+++ src/Entity/Department.php
@@ -22,5 +22,37 @@ class Deportament
      */
     private $name;
 
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
     
 }
